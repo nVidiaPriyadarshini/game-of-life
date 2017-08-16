a5bddc5e5e1df7f336108aa683754d76b57ad1cc
nVidia1 <galnextcomp@gmail.com>
2017-08-16 18:28:22 +0530

    adding interactive way to choose seed pattern and number of iterations

--- game-of-life/src/main/java/GenerateBoard.java
+++ game-of-life/src/main/java/GenerateBoard.java
@@ -9,6 +9,201 @@ public class GenerateBoard {
         return initializeRandomBoard(board);
     }
 
+    public static Board getBeehiveBoard() {
+        board = new Board(5, 6);
+        board.initializeBoard();
+        return initializeBeehiveBoard(board);
+
+    }
+
+    public static Board getLoafBoard() {
+        board = new Board(6, 6);
+        board.initializeBoard();
+        return initializeLoafBoard(board);
+    }
+
+    public static Board getBoatBoard() {
+        board = new Board(5, 5);
+        board.initializeBoard();
+        return initializeBoatBoard(board);
+    }
+
+    public static Board getTubBoard() {
+        board = new Board(5, 5);
+        board.initializeBoard();
+        return initializeTubBoard(board);
+    }
+
+    private static Board initializeTubBoard(Board board) {
+        board.setCellValue(1, 2, 1);
+        board.setCellValue(2, 1, 1);
+        board.setCellValue(2, 3, 1);
+        board.setCellValue(3, 2, 1);
+
+        return board;
+    }
+
+    private static Board initializeBoatBoard(Board board) {
+        board.setCellValue(1, 1, 1);
+        board.setCellValue(1, 2, 1);
+        board.setCellValue(2, 1, 1);
+        board.setCellValue(2, 3, 1);
+        board.setCellValue(3, 2, 1);
+
+        return board;
+    }
+
+    private static Board initializeLoafBoard(Board board) {
+        board.setCellValue(1, 2, 1);
+        board.setCellValue(1, 3, 1);
+        board.setCellValue(2, 1, 1);
+        board.setCellValue(2, 4, 1);
+        board.setCellValue(3, 2, 1);
+        board.setCellValue(3, 4, 1);
+        board.setCellValue(4, 3, 1);
+
+        return board;
+    }
+
+    private static Board initializeBeehiveBoard(Board board) {
+        board.setCellValue(1, 2, 1);
+        board.setCellValue(1, 3, 1);
+        board.setCellValue(2, 1, 1);
+        board.setCellValue(2, 4, 1);
+        board.setCellValue(3, 2, 1);
+        board.setCellValue(3, 3, 1);
+
+        return board;
+    }
+
+    public static Board getGliderBoard() {
+        board = new Board(6, 6);
+        board.initializeBoard();
+        return initializeGliderBoard(board);
+    }
+
+    public static Board getLeightWeightSpaceShip() {
+        board = new Board(7, 7);
+        board.initializeBoard();
+        return initializeLeightWeightSpaceShipBoard(board);
+    }
+
+    public static Board getToadBoard() {
+        board = new Board(6, 6);
+        board.initializeBoard();
+        return initializeToadBoard(board);
+    }
+
+    public static Board getPulsarBoard() {
+        board = new Board(15, 15);
+        board.initializeBoard();
+        return initializePulsarBoard(board);
+    }
+
+    private static Board initializePulsarBoard(Board board) {
+        board.setCellValue(1, 3, 1);
+        board.setCellValue(1, 4, 1);
+        board.setCellValue(1, 5, 1);
+        board.setCellValue(1, 9, 1);
+        board.setCellValue(1, 10, 1);
+        board.setCellValue(1, 11, 1);
+        board.setCellValue(3, 1, 1);
+        board.setCellValue(3, 6, 1);
+        board.setCellValue(3, 8, 1);
+        board.setCellValue(3, 13, 1);
+        board.setCellValue(4, 1, 1);
+        board.setCellValue(4, 6, 1);
+        board.setCellValue(4, 8, 1);
+        board.setCellValue(4, 13, 1);
+        board.setCellValue(5, 1, 1);
+        board.setCellValue(5, 6, 1);
+        board.setCellValue(5, 8, 1);
+        board.setCellValue(5, 13, 1);
+        board.setCellValue(6, 3, 1);
+        board.setCellValue(6, 4, 1);
+        board.setCellValue(6, 5, 1);
+        board.setCellValue(6, 9, 1);
+        board.setCellValue(6, 10, 1);
+        board.setCellValue(6, 11, 1);
+        board.setCellValue(8, 3, 1);
+        board.setCellValue(8, 4, 1);
+        board.setCellValue(8, 5, 1);
+        board.setCellValue(8, 9, 1);
+        board.setCellValue(8, 10, 1);
+        board.setCellValue(8, 11, 1);
+        board.setCellValue(9, 1, 1);
+        board.setCellValue(9, 6, 1);
+        board.setCellValue(9, 8, 1);
+        board.setCellValue(9, 13, 1);
+        board.setCellValue(10, 1, 1);
+        board.setCellValue(10, 6, 1);
+        board.setCellValue(10, 8, 1);
+        board.setCellValue(10, 13, 1);
+        board.setCellValue(11, 1, 1);
+        board.setCellValue(11, 6, 1);
+        board.setCellValue(11, 8, 1);
+        board.setCellValue(11, 13, 1);
+        board.setCellValue(13, 3, 1);
+        board.setCellValue(13, 4, 1);
+        board.setCellValue(13, 5, 1);
+        board.setCellValue(13, 9, 1);
+        board.setCellValue(13, 10, 1);
+        board.setCellValue(13, 11, 1);
+
+        return board;
+    }
+
+    private static Board initializeToadBoard(Board board) {
+        board.setCellValue(1, 2, 1);
+        board.setCellValue(1, 3, 1);
+        board.setCellValue(1, 4, 1);
+        board.setCellValue(2, 1, 1);
+        board.setCellValue(2, 2, 1);
+        board.setCellValue(2, 3, 1);
+        return board;
+    }
+
+    public static Board getBeaconBoard() {
+        board = new Board(6, 6);
+        board.initializeBoard();
+        return initializeBeaconBoard(board);
+    }
+
+    private static Board initializeBeaconBoard(Board board) {
+        board.setCellValue(1, 1, 1);
+        board.setCellValue(1, 2, 1);
+        board.setCellValue(2, 1, 1);
+        board.setCellValue(3, 4, 1);
+        board.setCellValue(4, 3, 1);
+        board.setCellValue(4, 4, 1);
+
+        return board;
+    }
+
+
+    private static Board initializeLeightWeightSpaceShipBoard(Board board) {
+        board.setCellValue(1, 2, 1);
+        board.setCellValue(1, 5, 1);
+        board.setCellValue(2, 1, 1);
+        board.setCellValue(3, 1, 1);
+        board.setCellValue(4, 1, 1);
+        board.setCellValue(4, 2, 1);
+        board.setCellValue(4, 3, 1);
+        board.setCellValue(4, 4, 1);
+        board.setCellValue(3, 5, 1);
+
+        return board;
+    }
+
+    private static Board initializeGliderBoard(Board board) {
+        board.setCellValue(2, 1, 1);
+        board.setCellValue(1, 3, 1);
+        board.setCellValue(2, 3, 1);
+        board.setCellValue(3, 2, 1);
+        board.setCellValue(3, 3, 1);
+        return board;
+    }
+
     public static Board getBlockBoard() {
 
         board = new Board(4, 4);
--- game-of-life/src/main/java/Main.java
+++ game-of-life/src/main/java/Main.java
@@ -1,13 +1,74 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
+    enum BoardType {
+        BLOCK, BEEHIVE, LOAF, BOAT, TUB, BLINKER, TOAD, BEACON, PULSAR, GLIDER, LWSS, RANDOM
+    }
 
-        Board board = GenerateBoard.getRandomizedFourCrossFourBoard();
-        Board nextStateOfBoard;
-        for(int i = 0 ; i< 100 ; i++) {
-            nextStateOfBoard = LifeCycle.getNextStateOfBoard(board);
-            nextStateOfBoard.printBoard();
-            board = nextStateOfBoard.clone();
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Choose from following seed board types :");
+        System.out.println("BLOCK,BEEHIVE,LOAF,BOAT,TUB,BLINKER,TOAD,BEACON,PULSAR,GLIDER,LWSS, RANDOM ");
+        BoardType boardType = BoardType.valueOf(reader.readLine());
+        System.out.println("Enter number of iterations");
+        int iterations = Integer.parseInt(reader.readLine());
+        Board board = null;
+        switch (boardType) {
+
+            case BLOCK:
+                board = GenerateBoard.getBlockBoard();
+                break;
+            case BEEHIVE:
+                board = GenerateBoard.getBeehiveBoard();
+                break;
+            case LOAF:
+                board = GenerateBoard.getLoafBoard();
+                break;
+            case BOAT:
+                board = GenerateBoard.getBoatBoard();
+                break;
+            case TUB:
+                board = GenerateBoard.getTubBoard();
+                break;
+            case BLINKER:
+                board = GenerateBoard.getBlinkerBoard();
+                break;
+            case TOAD:
+                board = GenerateBoard.getToadBoard();
+                break;
+            case BEACON:
+                board = GenerateBoard.getBeaconBoard();
+                break;
+            case PULSAR:
+                board = GenerateBoard.getPulsarBoard();
+                break;
+            case GLIDER:
+                board = GenerateBoard.getGliderBoard();
+                break;
+            case LWSS:
+                board = GenerateBoard.getLeightWeightSpaceShip();
+                break;
+            case RANDOM:
+                board = GenerateBoard.getRandomizedFourCrossFourBoard();
+                break;
+            default:
+                System.out.println("Valid pattern type was not entered");
+                break;
+
+        }
+
+        if (board != null) {
+            Board nextStateOfBoard;
+            for (int i = 0; i < iterations; i++) {
+                nextStateOfBoard = LifeCycle.getNextStateOfBoard(board);
+                nextStateOfBoard.printBoard();
+                board = nextStateOfBoard.clone();
+            }
         }
     }
 }
